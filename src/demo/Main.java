f91fc28af5f540ce4b32b163b4478d167390f084
MisuzuDate <138319041+maruri6797@users.noreply.github.com>
2023-11-21 14:36:44 +0900

    選択まで完了

---
+++ src/demo/Main.java
@@ -0,0 +1,71 @@
+package demo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import bases.Human;
+import bases.Monster;
+import humans.Brave;
+import humans.Fighter;
+import humans.Wizard;
+import monsters.Dragon;
+import monsters.Oak;
+import monsters.Slime;
+import utils.Dice;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("★★ ==== 戦いの開始だ！！ ==== ★★");
+		Brave brave = new Brave("沖田総司", "剣");
+		Fighter fighter = new Fighter("金太郎", "斧");
+		Wizard wizard = new Wizard("安倍晴明", "魔法");
+		List<Human> humans = new ArrayList<>();
+		humans.add(brave);
+		humans.add(fighter);
+		humans.add(wizard);
+		
+		Slime slime = new Slime("キングスライム", "体当たり");
+		Oak oak = new Oak("オークキング", "槍");
+		Dragon dragon = new Dragon("紅龍", "炎");
+		List<Monster> monsters = new ArrayList<>();
+		monsters.add(slime);
+		monsters.add(oak);
+		monsters.add(dragon);
+		
+		showGroupInfos(humans, monsters);
+		
+		int count = 1;
+		while (true) {
+			System.out.printf("\n★ 第%d回線 ==========\n", count);
+			System.out.println("\n[人間のターン！]\n");
+			choiceHuman(humans);
+			choiceMonster(monsters);
+			
+		}
+	}		
+		
+	public static Human choiceHuman(List<Human> humans) {
+		Human human = humans.get(Dice.get(0, humans.size() -1));
+		System.out.printf("人間グループから 「%s」 のお出ましだ！\n", human.getName());
+		return human;
+	}
+	
+	public static Monster choiceMonster(List<Monster> monsters) {
+		Monster monster = monsters.get(Dice.get(0, monsters.size() -1));
+		System.out.printf("モンスターグループから 「%s」 のお出ましだ！\n", monster.getName());
+		return monster;
+	}
+	
+	public static void showGroupInfos(List<Human> humans, List<Monster> monsters) {
+		System.out.println("\n## === グループ情報 === ##");
+		System.out.printf("#[人間グループ]: %d人\n", humans.size());
+		for (Human human : humans) {
+			System.out.println(human);
+		}
+		
+		System.out.printf("\n#[モンスターグループ]: %d人\n", monsters.size());
+		for (Monster monster : monsters) {
+			System.out.println(monster);
+		}
+	}
+}
---
+++ src/humans/Brave.java
@@ -0,0 +1,13 @@
+package humans;
+
+import bases.Human;
+import utils.Dice;
+
+public class Brave extends Human {
+
+	public Brave(String name, String weapon) {
+		super(name, weapon);
+		setHp(Dice.get(170, 230));
+		setOffensive(Dice.get(7, 13));
+	}
+}
